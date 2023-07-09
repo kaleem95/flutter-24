d254a0729dc6aadf4ff95e51e459b34f67b8ab9c
kaleem95 <kaleemalmomani@gmail.com>
2023-07-09 04:03:18 +0300

    Contain Solution For Task 1 and Task2

---
+++ session1/lib/task_one.dart
@@ -0,0 +1,22 @@
+void main() {
+
+  // To Convert From fahrenheit To Celsius
+  var fahrenheit = 50;
+  var celsius = (fahrenheit - 32) / 1.8;
+
+  print("The Result TO Convert from fahrenheit To Celsius is : $celsius");
+
+  print('-----------------------------');
+
+//   calculate area of circle
+
+  double pi = 3.14;
+
+  double r = 3;
+
+  double area = 0;
+
+  area = pi * r * r;
+
+  print("The Area Of Circle is : $area ");
+}
---
+++ session1/lib/task_two.dart
@@ -0,0 +1,83 @@
+void main() {
+
+  var fun = Parts();
+  int d = 50;
+  fun.grade(d);
+
+  print('--------------------');
+
+  var avg =PrintAverage();
+
+  double num1 =20;
+  double num2 =30;
+  double num3 =50;
+
+  avg.printAvg(num1,num2,num3);
+
+  print('--------------------');
+  var  funtwo =Compare();
+  int n1 =10 ;
+  int n2 =5 ;
+  funtwo.compareNum(n1, n2);
+
+
+ }
+
+
+
+
+class Parts {
+  void grade(int d) {
+    if (d > 0 && d <= 100) {
+      switch (d) {
+        case 10:
+          {
+            print('A');
+          }
+          break;
+        case 50:
+          {
+            print('B');
+          }
+          break;
+        case 60:
+          {
+            print('C');
+          }
+        case 100:
+          {
+            print('D');
+          }
+          break;
+      }
+    } else {
+      print('Please Enter Number Between 0  And  100');
+    }
+
+  }
+}
+
+
+class PrintAverage{
+
+
+  void printAvg(double num1, double num2 , double num3){
+    double sum = num1 + num2 + num3;
+
+    double average = sum /3 ;
+    print ('The Average is: $average');
+  }
+
+}
+
+class Compare{
+
+  void compareNum(int n1, int n2){
+
+    if(n1 > n2){
+      print('Yes');
+      }
+    else
+      print('No');
+  }
+}
